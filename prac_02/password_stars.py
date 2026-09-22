bd598ac0d7ed6e5f0e6c144ecb658f505ba6464d
freddie beldam <freddie_1989@hotmail.com>
2026-09-22 12:38:59 +1000

    Add corrected password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,32 +1,18 @@
-"""
-program that asks the user for a low and a high number,
-ensuring the high is higher than the low.
-then print n smiley faces :) where n is a random number between low and high inclusive.
-"""
-from random import randint
-
-# get number (high) and get number (low)
-# check the high number is actually higher than the lower
-# select a random nmber between low and
-# high inlcusive and print that amount of smiley faces (n)
-
 def main():
-    low = int(input("Enter low number: "))
-    high = int(input("Enter high number: "))
+    password = get_password()
+    print_stars(password)
 
-# ensure high is higher than low
-    while high <= low:
 
-        print("High number must be greater than low number.")
-        high = int(input("Enter high number: "))
+def get_password():
+    password = input("Enter password: ")
+    while len(password) == 0:
+        print("Password cannot be empty.")
+        password = input("Enter password: ")
+    return password
 
-# Pick random number between low and high inclusive
-    n = randint(low, high)
 
-# print smiley faces
-    print("Here are your smiles baby: ")
-    print(":)" * n)
+def print_stars(password):
+    print("*" * len(password))
+
 
 main()
-
-
